0d9c3e74a4d9b3b13a76cf1ab5fdf97ebe871363
Mohamed Mosalm <mohamed.mosalm2002@gmail.com>
2024-11-29 18:27:56 +0200

    feat: Implement user registration service with password encryption

---
+++ user-service/src/main/java/org/protu/userservice/service/UserService.java
@@ -0,0 +1,40 @@
+package org.protu.userservice.service;
+
+import org.protu.userservice.dto.RegisterRequestDTO;
+import org.protu.userservice.model.User;
+import org.protu.userservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(RegisterRequestDTO registerRequest) {
+        if (userRepository.existsByUsername(registerRequest.getUsername())) {
+            throw new IllegalArgumentException("Username is already taken");
+        }
+
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new IllegalArgumentException("Email is already registered");
+        }
+
+        User user = new User();
+        user.setFirstName(registerRequest.getFirstName());
+        user.setLastName(registerRequest.getLastName());
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPasswordHash(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setPhoneNumber(registerRequest.getPhoneNumber());
+        user.setAuthorities("ROLE_USER");
+
+        return userRepository.save(user);
+    }
+}
