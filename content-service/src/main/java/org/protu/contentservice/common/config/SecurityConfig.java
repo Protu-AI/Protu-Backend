b951200aca652ce6990007dc9e713c01f199b833
hazem <hazemelakbawy62@gmail.com>
2025-04-23 01:37:12 +0200

    fix(content-service): Add CORS configurations to allow frontend requests

--- content-service/src/main/java/org/protu/contentservice/common/config/SecurityConfig.java
+++ content-service/src/main/java/org/protu/contentservice/common/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
         .csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .authorizeHttpRequests(configurer -> configurer
             .requestMatchers(HttpMethod.GET).permitAll()
             .anyRequest().authenticated()
